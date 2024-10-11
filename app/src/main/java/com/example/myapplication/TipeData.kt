3b3a5f90e19fde09008fef45308c0b86a53959f7
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-11 12:26:30 +0700

    Belajar membuat variabel beserta tipe data dan isinya yaitu double, char, boolean, string.

--- app/src/main/java/com/example/myapplication/TipeData.kt
+++ app/src/main/java/com/example/myapplication/TipeData.kt
@@ -20,4 +20,23 @@ fun main() {
     // Float
     val floatValue: Float = 50.6f
     println("Float: $floatValue")
+
+    // Double
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    // Char
+    val charValue: Char = 'A'
+    println(charValue)
+
+    // Boolean
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    // String
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin"
+    println("string: $stringValue")
 }
