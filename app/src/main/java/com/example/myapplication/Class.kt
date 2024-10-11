c53f9a3bd273d24ca6fbd54957f01355ac2aa57d
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-11 12:29:53 +0700

    membuat class dan menampilkannya

---
+++ app/src/main/java/com/example/myapplication/Class.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+class Motor()
+
+class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "@mary@gmail.com")
+
+    // Prints the value of the property: email
+    println(contact.email)
+}
