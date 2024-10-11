f6777e83c98e6d1ae7823dffc4cac19acab69edf
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-11 12:24:45 +0700

    Belajar membuat variabel beserta tipe data dan isinya yaitu byte, short, int long, float.

---
+++ app/src/main/java/com/example/myapplication/TipeData.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+fun main() {
+    // Byte
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // Short
+    val shortValue: Short = 20
+    println("Short: $shortValue")
+
+    // Int
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    // Long
+    val longValue: Long = 48
+    println("Long : $longValue")
+
+    // Float
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+}
